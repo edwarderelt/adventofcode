6afa39aa31f050455e2c213d547bb82fd7cfec02
Edward Erelt <edward@erelt.net>
2024-12-11 14:17:45 +0000

    Add 2024 day 10

---
+++ 2024/10/main_02.py
@@ -0,0 +1,28 @@
+matrix = []
+with open("10/input.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        matrix.append(list(map(int, line.strip())))
+
+print(matrix)
+
+def get_trailheads(i, j):
+    if matrix[i][j] == 9:
+        return 1
+    total_trailheads = 0
+    if i > 0 and matrix[i-1][j] == matrix[i][j] + 1:
+        total_trailheads+=get_trailheads(i-1, j)
+    if i < len(matrix)-1 and matrix[i+1][j] == matrix[i][j] + 1:
+        total_trailheads+=get_trailheads(i+1, j)
+    if j > 0 and matrix[i][j-1] == matrix[i][j] + 1:
+        total_trailheads+=get_trailheads(i, j-1)
+    if j < len(matrix[i])-1 and matrix[i][j+1] == matrix[i][j] + 1:
+        total_trailheads+=get_trailheads(i, j+1)
+    return total_trailheads
+
+total = 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] == 0:
+            total += get_trailheads(i, j)
+print(total)
